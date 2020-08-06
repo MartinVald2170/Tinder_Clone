ab67dcdffee843a7062e1cf2cf31b4186e5178da
MartinVald2170 <noways2170@gmail.com>
2020-08-06 14:50:05 +1000

    header done

---
+++ tinder-clone/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tinder Clone</h1>
+
+    {/* Header */}
+    <Header />
+    {/* Tinder Cards */}
+    {/* Buttons Below tinder cards*/ }
+    {/* Chats Screen */}
+    {/* Individual chat screen */}
+
+    </div>
+  );
+}
+
+export default App;
